4210c08e931615e4f49521b74eb2c5c23d84714b
Fahimreza412 <fahimreza2210622123@gmail.com>
2024-10-07 01:04:28 +0600

    3.1

---
+++ src/script.js
@@ -0,0 +1,3 @@
+function handleMenu() {
+    navDialog.classList.toggle('hidden');
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-image': "url('/src/assets/bg.jpg')",
+      }
+    },
   },
   plugins: [ require('daisyui'),],
 }
